e7d3cc08ade870cd3a5e242ff3f8d223f7ef550f
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-04-23 13:09:55 +0200

    transfer product section to ProductContent.vue and the corrisponding css to _product.scss partials

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -6,36 +6,25 @@
         </div>
     </div>
 
-    <AwardsContent/>
+    <AwardsContent />
     <div class="container">
         <div class="services">
             <h4 class="service-title">MENS GROOMING</h4>
             <h1 class="service-description">Services</h1>
         </div>
         <div class="row">
-            
 
-            <ServicesContent v-for="(item,i) in service" :key="i" :item="item"/>        
+
+            <ServicesContent v-for="(item, i) in service" :key="i" :item="item" />
 
         </div>
         <div class="our-services">READ ABOUT OUR SERVICES</div>
     </div>
     <img src="/img/triangle.svg" alt="" class="triangle bg-triangle">
-    <div class="bg-products">
-        <h3>WE HAVE YOU COVERED</h3>
-        <h1>Avada Grooming Products</h1>
-        <div class="row">
-        <div class="card" v-for="i in 4 " :key="i">
-            <img :src="`${products[i].img}`" alt="">
-            <h2>{{ products[i].name }}</h2>
-            <p>{{ products[i].price }}</p>
-        </div>
-        </div>
-        
-    </div>
     
+    <ProductsContent/>
 
-    
+
 
 </template>
 
@@ -43,19 +32,21 @@
 import HeroContent from '../components/HeroContent.vue'
 import AwardsContent from '../components/AwardsContent.vue'
 import ServicesContent from '../components/ServicesContent.vue'
-import {db} from '../store.js'
+import ProductsContent from '../components/ProductsContent.vue'
+import { db } from '../store.js'
 export default {
-    data(){
-        return{
-            service:db.services,
-            products:db.products
+    data() {
+        return {
+            service: db.services,
+           
         }
 
     },
     components: {
         HeroContent,
         AwardsContent,
-        ServicesContent
+        ServicesContent,
+        ProductsContent
     }
 
 }
@@ -63,10 +54,6 @@ export default {
 
 <style lang="scss" scoped>
 @use '../style/partials/main.scss';
-.bg-products{
-    background-image: url('/img/avadabarbers-reviewsbackground.jpg');
-   color: white;
-}
 
 
 </style>
---
+++ src/components/ProductsContent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="bg-products">
+        <h3 class="products-title">WE HAVE YOU COVERED</h3>
+        <h1 class="products-description">Avada Grooming Products</h1>
+        <div class="container-products">
+            <div class="row">
+                <div class="card-products" v-for="i in 4  " :key="i">
+                    <img :src="`${products[i - 1].img}`" alt="">
+                    <h2>{{ products[i - 1].name }}</h2>
+                    <p>{{ products[i - 1].price }}</p>
+                </div>
+
+            </div>
+            <p class="our-services">SHOP OUR PRODUCT RANGE</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { db } from '../store.js'
+    export default {
+        data() {
+        return {
+            
+            products: db.products
+        }
+
+    },
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../style/partials/products.scss';
+</style>
