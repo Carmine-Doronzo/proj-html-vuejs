61bdaab15db52055324c80e7c82ffc2480509012
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-04-23 18:26:21 +0200

    Transfer carousel in the productCarousel Content.vue and the correspondng css to partials _carousel.scss

---
+++ src/components/ProductsCarouselContent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container-carousel">
+
+        <div class="row-carousel">
+            <div class="col-4">
+                <img src="/img/shaving_butter-800x800.png" alt="">
+            </div>
+            <div class="col-8">
+                <h4 class="orange-color"> THIS MONTHS FEATURED PRODUCT</h4>
+                <h1>Shaving Butter</h1>
+                <p class="grey-color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ducimus officiis
+                    vitae doloribus repellendus neque eligendi iste, dolor recusandae qui deserunt earum ab a et
+                    provident saepe quam!</p>
+                <div class="orange-color button-carousel">BUY NOW</div>
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../style/partials/carousel.scss';
+</style>
